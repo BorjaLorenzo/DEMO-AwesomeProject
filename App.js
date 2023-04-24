80053ef67a0cf95fd67f510e762c10f094b72be8
borjalorenzolcdlc <borjalb98@gmail.com>
2023-04-24 15:35:08 +0200

    dia 2
    falta centrar el boton de login

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity } from 'react-native';
 import React, { useState } from 'react';
-import { isMobile } from 'react-device-detect';
+import { Dimensions, Platform } from 'react-native';
 
 
 const styles = StyleSheet.create({
@@ -10,34 +10,69 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    width: '100%',
+    fontSize: 25
+  },
+  containerLogin: {
+    width: '33%',
+    padding: '3%',
+    display: 'flex',
+    alignItems: 'center',
+    borderRadius: 25,
+    backgroundColor: 'powderblue'
+  },
+  containerLoginMobile: {
+    width: '80%',
+    display: 'flex',
+    alignItems: 'center'
   },
   inputMobile: {
     textAlign: 'center',
-    width: '50%',
+    width: '100%',
     margin: 12,
     borderWidth: 1,
     padding: 10,
-    color:'red'
   },
   input: {
     textAlign: 'center',
-    width: '15%',
+    width: '100%',
     margin: 12,
     borderWidth: 1,
     padding: 10,
-    color:'green'
   },
-  button: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
+  boton: {
+    width: '50%',
+    marginTop: '5%',
+  },
+  botonTexto: {
+    fontSize: 25
   },
   tituloAzul: {
     color: 'navy'
-  }
-});
+  },
+  h1: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: 'blue',
+    paddingVertical: 12,
 
+    borderRadius: 10,
+    elevation: 3,
+    width:'150%'
+  },
+  buttonText: {
+    fontSize: 16,
+    color: '#fff',
+    alignSelf: 'center',
+    textTransform: 'uppercase',
+  },
+});
+const { width } = Dimensions.get('window');
+const isMobile = width < 768;
+const deviceType = isMobile ? 'Mobile' : 'Desktop';
 const RegisterForm = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -46,39 +81,46 @@ const RegisterForm = () => {
   const handleRegister = () => {
     // Código para enviar los datos del formulario
   };
-  const estiloPantalla = () => {
-    if (isMobile) {
-      return styles.input
-    } else {
+  var inputPantalla = () => {
+    if (deviceType == "Mobile") {
       return styles.inputMobile
+    } else {
+      return styles.input
     }
   }
-
+  var containerLoginPantalla = () => {
+    if (deviceType == "Mobile") {
+      return styles.containerLoginMobile
+    } else {
+      return styles.containerLogin
+    }
+  }
   return (
     <View style={styles.container}>
-      <Text style={styles.tituloAzul}>INCOES</Text>
-      <Text>Ingeniería y Control de estacionamientos</Text>
+      <View style={containerLoginPantalla()}>
+        <Text style={StyleSheet.flatten([styles.tituloAzul, styles.h1])}> INCOES</Text>
+        <Text>Ingeniería y Control de estacionamientos</Text>
 
 
-      <TextInput
-        style={estiloPantalla()}
-        placeholder="Usuario"
-        value={username}
-        onChangeText={setUsername}
-      />
-      <TextInput
-        style={estiloPantalla()}
-        placeholder="Contraseña"
-        value={password}
-        onChangeText={setPassword}
-      />
-      {/* <TextInput
-        style={styles.input}
-        placeholder="Email"
-        value={email}
-        onChangeText={setEmail}
-      /> */}
-      <Button title="Entrar" onPress={handleRegister} />
+        <TextInput
+          style={inputPantalla()}
+          placeholder="Usuario"
+          value={username}
+          onChangeText={setUsername}
+        />
+        <TextInput
+          style={inputPantalla()}
+          placeholder="Contraseña"
+          value={password}
+          onChangeText={setPassword}
+        />
+        <View style={styles.boton}>
+          {/* <Button title="Entrar" onPress={handleRegister} style={styles.boton} titleStyle={styles.botonTexto}/> */}
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Login</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 };
